cb0febc6118a8f30c7ea87ac10b755cf73b30485
Mrpotatoe22 <726318@student.dusd.net>
2025-03-28 09:57:50 -0700

    Added aggressive animals
    finishing challenges

--- BasicGameplaySanchezP2/Assets/Scripts/DetectCollisions.cs
+++ BasicGameplaySanchezP2/Assets/Scripts/DetectCollisions.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class DetectCollisions : MonoBehaviour
 {
+    private GameManager gameManager;
     // Start is called before the first frame update
     void Start()
     {
-
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
