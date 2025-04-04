a31a3623c6d89ab60e77d80d9107a5b654f06f43
Mrpotatoe22 <726318@student.dusd.net>
2025-04-04 09:44:38 -0700

    Finished Challenge 2
    Done

--- BasicGameplaySanchezP2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ BasicGameplaySanchezP2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -10,6 +10,7 @@ public class SpawnManagerX : MonoBehaviour
     private float spawnLimitXRight = 7.0f;
     private float spawnPosY = 30.0f;
 
+    private float delayBeforeSpawn = 3f;
     private float startDelay = 1.0f;
     private float spawnInterval = 4.0f;
 
@@ -20,16 +21,31 @@ public class SpawnManagerX : MonoBehaviour
     }
 
     // Spawn random ball at random x position at top of play area
-    void SpawnRandomBall ()
-    {
-        //Selects ball for spawning
-        int randomBall = Random.Range(0, ballPrefabs.Length);
 
+    private void Update()
+    {
+        //checking if our time to spawn ball is passed
+        if (delayBeforeSpawn <= 0)
+        {
+            //If true spawn a ball and generate a random time
+            SpawnRandomBall();
+            delayBeforeSpawn = Random.Range(3, 5);
+        }
+        else
+        {
+            //If false reducing the time before spawn. Using Time.deltaTime because it can simulate a real countdown.
+            delayBeforeSpawn -= Time.deltaTime;
+        }
+    }
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall()
+    {
         // Generate random ball index and random spawn position
+        int ballIndex = Random.Range(0, ballPrefabs.Length);
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 
         // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[randomBall], spawnPos, ballPrefabs[0].transform.rotation);
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[0].transform.rotation);
     }
 
 }
